33228759446f79070b0b232531f81e9597fec129
dieudx <126355292+dieudx@users.noreply.github.com>
2023-03-01 19:55:59 +0530

    Update configs.py

--- configs.py
+++ configs.py
@@ -1,12 +1,12 @@
 from os import path, getenv
 
 class Config:
-    API_ID = int(getenv("API_ID", "0112234"))
-    API_HASH = getenv("API_HASH", "abcdefg")
-    BOT_TOKEN = getenv("BOT_TOKEN", "1234567891:AdDfgFRFVVfDEhdhyjjvjjftSEW")
-    FSUB = getenv("FSUB", "SDBotz")
-    CHID = int(getenv("CHID", "-1000112234"))
-    SUDO = list(map(int, getenv("SUDO").split()))
-    MONGO_URI = getenv("MONGO_URI", "")
+    API_ID = int(getenv("API_ID", "21967179"))
+    API_HASH = getenv("API_HASH", "37d5fbb9217cb56d6261e61305d92748")
+    BOT_TOKEN = getenv("BOT_TOKEN", "")
+    FSUB = getenv("FSUB", "-1001836265597")
+    CHID = int(getenv("CHID", ""))
+    SUDO = list(map(int, getenv("SUDO", "5630387958").split()))
+    MONGO_URI = getenv("MONGO_URI", "mongodb+srv://kailastg:<kailastg>@cluster0j.eb3ewbt.mongodb.net/?retryWrites=true&w=majority")
     
 cfg = Config()
